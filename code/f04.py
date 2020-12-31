d39ccdeb0607df88ecda4157946784f1d05d898e
Shamik69 <k.shanikpal@gmail.com>
2020-12-31 15:34:36 +0530

    parameter adjustments

--- code/f04.py
+++ code/f04.py
@@ -1,9 +1,35 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-path = 'C:/Users/User/PycharmProjects/cobweb-model'
+n= 100
+a0= -588282
+a1= 2
+ratio_exp= 0.95
+y0= []
+y1= []
+y2= []
+x= []
+err= ratio_exp-abs(a0/a1)
+print(err)
+for i in range(n):
+    ratio= abs(a1/a0)
+    a0+=err
+    y0+=[a0]
+    err= ratio_exp-abs(a1/a0)
+    y1+=[a1]
+    y2+=[ratio]
+    x+=[i]
+    if ratio< ratio_exp:
+        print(i)
+        break
+print(f'{y0}\n{y1}\n{y2}')
+y0= np.array(y0)-np.array(y0).mean()
+y1= np.array(y1)-np.array(y1).mean()
+y0= np.array(y2)-np.array(y2).mean()
+y0= np.array(y0)-np.array(y0).mean()
 
-df = pd.read_csv(f'{path}/data/cobweb.csv')
-
-plt.scatter(df.quantity, df.price)
+plt.plot(x, y0, label= 'a0')
+plt.plot(x, y1, label= 'a1')
+plt.plot(x, y2, label= 'ratio')
+plt.legend()
 plt.show()
--- code/f05.py
+++ code/f05.py
@@ -9,7 +9,7 @@ considering a linear demand and supply curve
 '''
 
 
-def cobweb(a0: float, b0: float, a1: float, b1: float, p0: float = 0, x0: float, t: float):
+def cobweb(a0: float, b0: float, a1: float, b1: float, x0: float, t: float, p0: float = 0):
     '''
     given variables
     a0: intercept of the demand curve(+ve)
@@ -17,12 +17,15 @@ def cobweb(a0: float, b0: float, a1: float, b1: float, p0: float = 0, x0: float,
     a1: intercept of the supply curve(-ve)
     b1: slope of the supply curve(+ve)
     t: period
-    x0: period 0 price
+    x0: period0 price
 
     generated variables
-    x1: period 1 price
+    x1: period1 price
     pp: intertemporal eqm price
     time_path: time path
+    dd: eqm demand
+    ss: eqm supply
+    in eqm condition dd and ss should and would be the same
     '''
 
     x1 = ((a1 - a0) + (b1 * x0))/b0
@@ -30,7 +33,8 @@ def cobweb(a0: float, b0: float, a1: float, b1: float, p0: float = 0, x0: float,
     ss = a1 + b1 * x0
     pp = -((a0 - a1) / (b0 - b1))
     time_path = ((p0 - pp) * (b0 / b1) ** t) + pp
-    return [x1, pp, time_path]
+    
+    return [x1, pp, dd, ss, time_path]
 
 
 def loop(loop_limit: int):
@@ -49,7 +53,16 @@ def loop(loop_limit: int):
         if b<0:
             b*=-1
         if b>1:
-            loops= 100
+            ratio_exp= 0.95
+            b1= params[3]
+            err= ratio_exp-b
+            n= 100
+            for i in range(n):
+                b1+=err
+                err= ratio_exp-b
+                if b< ratio_exp:
+                    break
+        params[1]= b1
         # calling data generator
         x0= 0
         for i in range(loops):
@@ -59,5 +72,3 @@ def loop(loop_limit: int):
             t= i, x0= x0, p0= x0)
             x0= data[0]
             data_storage.append(data)
-        # manipulaing parameters
-        
