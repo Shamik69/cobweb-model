5d14535b3e1827a0356374bc2aee7cc601fc5249
Shamik69 <k.shanikpal@gmail.com>
2020-12-31 12:04:13 +0530

    matlab replaced with octave code

--- code/f07.m
+++ code/f07.m
@@ -7,12 +7,13 @@ a0=1;
 b0=-2;
 a1=-0.5;
 b1=1.95;
+new_round = @(x, n=0) round (x * 10^n) * 10^(-n)
 while n<=500
     p0(length(p0)+1)= x0;
     x1= ((a1 - a0) + (b1 .* x0))/b0;
     p1(length(p1)+1)= x1;
     t(length(t)+1)=n;
-    if round(x0, 2)== round(x1, 2)
+    if new_round(x0, 2)== new_round(x1, 2)
         break
     end
     x0=x1;
@@ -28,28 +29,28 @@ fig1= figure(1);
 plot(t, time_path)
 xlabel('periods')
 ylabel('time path')
-saveas(fig1,'C:/Users/User/PycharmProjects/cobweb-model/figs/periods-time path(matplotlib).png')
+saveas(fig1,'C:/Users/User/PycharmProjects/cobweb-model/figs/periods-time path(octave).png')
 
 fig2= figure(2);
 plot(t, dd)
 xlabel('periods')
 ylabel('demand')
-saveas(fig2,'C:/Users/User/PycharmProjects/cobweb-model/figs/periods-demand(matplotlib).png')
+saveas(fig2,'C:/Users/User/PycharmProjects/cobweb-model/figs/periods-demand(octave).png')
 
 fig3= figure(3);
 plot(t, p1)
 xlabel('periods')
 ylabel('price')
-saveas(fig3,'C:/Users/User/PycharmProjects/cobweb-model/figs/periods-price(matplotlib).png')
+saveas(fig3,'C:/Users/User/PycharmProjects/cobweb-model/figs/periods-price(octave).png')
 
 fig4= figure(4);
 plot(time_path, p1)
 ylabel('price')
 xlabel('time path')
-saveas(fig4,'C:/Users/User/PycharmProjects/cobweb-model/figs/time path-price(matplotlib).png')
+saveas(fig4,'C:/Users/User/PycharmProjects/cobweb-model/figs/time path-price(octave).png')
 
 fig5= figure(5);
 plot(time_path, dd)
 ylabel('demand')
 xlabel('time path')
-saveas(fig5,'C:/Users/User/PycharmProjects/cobweb-model/figs/time path-demand(matplotlib).png')
+saveas(fig5,'C:/Users/User/PycharmProjects/cobweb-model/figs/time path-demand(octave).png')
